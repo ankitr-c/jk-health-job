0a7ff1363362f62c171e106cb9bca2219112e50b
Ankit <ankit.raut@coditas.com>
2024-01-24 00:17:47 +0530

    debuging added to script

--- script.sh
+++ script.sh
@@ -6,7 +6,8 @@ if [ -z "$ip_addr" ]; then
   echo "IP address not provided"
   exit 1
 fi
-
+result=$(gcloud compute instances list --filter="networkInterfaces.accessConfigs.natIP:${ip_addr} OR networkInterfaces.networkIP:${ip_addr}" | awk 'NR==1 {print $1, $NF; exit}'
+echo $result
 result=$(gcloud compute instances list --filter="networkInterfaces.accessConfigs.natIP:$ip_addr OR networkInterfaces.networkIP:$ip_addr" --format="value(status)")
 
 if [ -z "$result" ]; then
