3642ece0a4535b26869da3124ea50a5b26d2b05e
Ankit <ankit.raut@coditas.com>
2024-01-24 00:09:13 +0530

    script syntax fixed

--- script.sh
+++ script.sh
@@ -1,16 +1,18 @@
-ip_addr=$1
-result=$(gcloud compute instances list --filter="networkInterfaces.accessConfigs.natIP:${ip_addr} OR networkInterfaces.networkIP:${ip_addr}" | awk 'NR>1 {print $NF}')
+#!/bin/bash
 
-if [ -z "$ip_addr" ]
-    then
-        echo "IP address not Provided"
-        exit 1
-elif [ $result=="RUNNING" ] 
-then
-    echo "Status is: RUNNING"
-elif [ $result=="TERMINATED" ]
-then
-    echo "Status is: STOPPED"
-else
-    echo "Status is: DELETED or VM Unavailabe"
+ip_addr=$1
+
+if [ -z "$ip_addr" ]; then
+  echo "IP address not provided"
+  exit 1
+fi
+
+result=$(gcloud compute instances list --filter="networkInterfaces.accessConfigs.natIP:$ip_addr OR networkInterfaces.networkIP:$ip_addr" --format="value(status)")
+
+if [ -z "$result" ]; then
+  echo "Status is: DELETED or VM Unavailable"
+elif [ "$result" = "RUNNING" ]; then
+  echo "Status is: RUNNING"  
+elif [ "$result" = "TERMINATED" ]; then
+  echo "Status is: STOPPED"
 fi
