fe434b298282ff31d83f5466c566ecea5dc3879c
Ankit <ankit.raut@coditas.com>
2024-01-23 23:51:43 +0530

    script modified

--- script.sh
+++ script.sh
@@ -1,2 +1,16 @@
 ip_addr=$1
-gcloud compute instances list --filter="networkInterfaces.accessConfigs.natIP:${ip_addr} OR networkInterfaces.networkIP:${ip_addr}" | awk 'NR>1 {print $NF}'
+result=$(gcloud compute instances list --filter="networkInterfaces.accessConfigs.natIP:${ip_addr} OR networkInterfaces.networkIP:${ip_addr}" | awk 'NR>1 {print $NF}')
+
+if [ -z "$ip" ]
+    then
+        echo "IP address not Provided"
+        exit 1
+elif [ $result=="RUNNING" ] 
+then
+    echo "Status is: RUNNING"
+elif [ $result=="TERMINATED" ]
+then
+    echo "Status is: STOPPED"
+else
+    echo "Status is: DELETED or VM Unavailabe"
+fi
